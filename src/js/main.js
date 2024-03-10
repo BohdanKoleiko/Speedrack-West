f1974795535841b77df25eb162189bf9834fdaa3
HamaHs <lusuy1995@gmail.com>
2024-03-10 15:44:21 +0100

    adde script that sets additional option inputs as unchecked by default

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const initCheckBoxes = function (checkbox) {
+   checkbox.dataset.checked = "";
+   for (let item of checkbox.children) {
+      if (item.tagName === "INPUT") {
+         item.checked = false;
+      }
+   }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+   const checkboxes = document.querySelectorAll(".input");
+
+   console.log(checkboxes);
+
+   checkboxes.forEach((checkbox) => {
+      initCheckBoxes(checkbox);
+
+      checkbox.addEventListener("click", function (e) {
+         e.preventDefault;
+
+         for (let item of checkbox.children) {
+            if (item.tagName === "INPUT") {
+               if (item.checked) {
+                  item.checked = false;
+                  checkbox.dataset.checked = item.checked;
+               } else {
+                  item.checked = true;
+                  checkbox.dataset.checked = item.checked;
+               }
+            }
+         }
+      });
+   });
+});
